26bc7854b3ec49c81e3549e009aad7e8c111f4dd
AniaLaz <lazarenko.ania.sg@gmail.com>
2022-09-09 22:49:21 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,45 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientsEl = document.querySelector('#ingredients'); 
+
+
+// const elements = ingredients.map(
+//   ingredient => {
+//     const listEl = document.createElement('li');
+//     listEl.classList.add('item');
+//     listEl.textContent = ingredient;
+//     console.log(ingredient);
+//       console.log(listEl);
+//       return listEl
+//   }
+// );
+// ingredientsEl.append(...elements);
+
+
+
+const makeIngredients = options =>{
+  return options.map (
+    option => {
+          const listEl = document.createElement('li');
+          listEl.classList.add('item');
+          listEl.textContent = option;
+          console.log(option);
+          console.log(listEl);
+          return listEl;
+        });
+};
+
+const elements = makeIngredients(ingredients);
+ingredientsEl.append(...elements);
+
+
+
+
+
+
+
+
+
