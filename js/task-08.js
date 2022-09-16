67dc7d9639f8ee5a2ea02f1ce5a8e0cead331af2
AniaLaz <lazarenko.ania.sg@gmail.com>
2022-09-16 10:43:43 +0300

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const formEl = document.querySelector('.login-form');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+
+function onFormSubmit(event) {
+        event.preventDefault();
+const {email,password} = event.currentTarget.elements;
+
+if(email.value === "" || password.value === ""){
+    alert('Pleasa fill in all the fields');
+}
+console.log(event.currentTarget.elements);
+
+const result = {
+    email: email.value, 
+    password: password.value
+};
+
+// console.log(`${email.name}: ${email.value}, ${password.name}: ${password.value}`);
+console.log(result);
+alert('Registration was successful');
+event.currentTarget.reset();
+
+    }
 
