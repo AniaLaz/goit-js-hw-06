55d91978fad3a3e011371c15b4729bc81a4ad18f
AniaLaz <lazarenko.ania.sg@gmail.com>
2022-09-18 08:56:32 +0300

    task-5

--- js/task-05.js
+++ js/task-05.js
@@ -8,4 +8,7 @@ refs.input.addEventListener ('input', onInputChenge);
 function onInputChenge(event) {
     console.log(event.currentTarget.value);
     refs.nameOutput.textContent = event.currentTarget.value.trim();
+    if (event.currentTarget.value===""){
+        refs.nameOutput.textContent = "Anonymous"
+    }
 }
